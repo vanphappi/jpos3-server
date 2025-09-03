e56ede681e9a06670feef488fa2762b9eb60a69f
vinh <vinh.mx@atomsolution.com.vn>
2025-09-03 17:11:53 +0700

    up

--- jpos-server/src/main/java/com/cardswitch/application/service/FraudService.java
+++
@@ -1,7 +0,0 @@
-package com.cardswitch.application.service;
-
-import com.cardswitch.domain.transaction.Transaction;
-
-public interface FraudService {
-    boolean isSuspicious(Transaction transaction);
-}
--- jpos-server/src/main/java/com/cardswitch/application/service/RoutingService.java
+++
@@ -1,7 +0,0 @@
-package com.cardswitch.application.service;
-
-import com.cardswitch.domain.transaction.Transaction;
-
-public interface RoutingService {
-    String route(Transaction transaction);
-}
